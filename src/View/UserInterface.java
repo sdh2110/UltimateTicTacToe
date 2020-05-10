74ab3b7b492f79244c8c152a3010b7a98e216dc3
Steven Hulbert <sdh2110@g.rit.edu>
2020-05-10 14:58:20 -0400

    Created interface for UIs

---
+++ src/View/UserInterface.java
@@ -0,0 +1,14 @@
+package View;
+
+/**
+ * An interface for all UIs used by Tic-Tac-Toe games.
+ * 
+ * @author Steven Hulbert
+ */
+public interface UserInterface {
+    
+    /**
+     * Updates the user interface.
+     */
+    public void update();
+}
