fa78be3ba63615f7662aba10fab130b03004c620
Steven Hulbert <sdh2110@g.rit.edu>
2020-05-10 12:16:24 -0400

    Created new interface for board components
    This is to fully utilize the composite pattern.

---
+++ src/Model/BoardComponent.java
@@ -0,0 +1,27 @@
+package Model;
+
+import Controller.GameMoves.MoveData;
+
+/**
+ * An interface for the components that make up a Tic-Tac-Toe board.
+ * 
+ * @author Steven Hulbert
+ */
+public interface BoardComponent {
+
+    /**
+     * Creates a new BoardComponent of this component's type.
+     * 
+     * @return a new BoardComponent
+     */
+    public BoardComponent createNewComponent();
+    
+    /**
+     * Attempts the given move on this component.
+     * 
+     * @param move - The information for the move that is being attempted
+     * @return whether or not the move was a valid move
+     */
+    public boolean attemptMove(MoveData move);
+    
+}
