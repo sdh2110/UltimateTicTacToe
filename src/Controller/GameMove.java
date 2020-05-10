b0394561fe55971fa7942a9f261399baaaa46e57
Steven Hulbert <sdh2110@g.rit.edu>
2020-05-10 08:38:23 -0400

    Changed GameMove to an interface
    Modified it to prepare it to become a decorated object

--- src/Controller/GameMove.java
+++ src/Controller/GameMove.java
@@ -1,32 +1,42 @@
 package Controller;
 
+import Model.PieceType;
+import Model.Space;
+
 /**
  * A packet containing the information for a move made in a Tic-Tac-Toe game.
  * 
  * @author Steven Hulbert
  */
-public class GameMove {
-    
-    // The x-coordinate at which this move is being attempted
-    private int xCoordinate;
-    // The y-coordinate at which this move is being attempted
-    private int yCoordinate;
+public interface GameMove {
 
     /**
      * Gets the x-coordinate at which this move is being attempted.
      * 
      * @return the x-coordinate
      */
-    public int getXCoordinate() {
-        return xCoordinate;
-    }
+    public int getXCoordinate();
 
     /**
      * Gets the y-coordinate at which this move is being attempted.
      * 
      * @return the t-coordinate
      */
-    public int getYCoordinate() {
-        return yCoordinate;
+    public int getYCoordinate();
     }
+    
+    /**
+     * Gets the type of piece to place with this move.
+     * 
+     * @return the piece type
+     */
+    public PieceType getPieceType();
+
+    /**
+     * Get the inner bit of data stored in this GameMove containing the next
+     * information required for processing this move.
+     * 
+     * @return gets the next packet of data for this move
+     */
+    public GameMove getNextData();
  }
